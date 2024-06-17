65c9c634fdf72c974a39ec78746b4fd2230401b6
Eric Cheung <eric1162004@hotmail.com>
2024-06-17 13:50:47 -0700

    iterative binary search

--- Searching/binary_search.ts
+++ Searching/binary_search.ts
@@ -23,7 +23,7 @@ function _recursiveBinarySearch(
   left: number,
   right: number
 ): number {
-  if (left > right) return -1; 
+  if (left > right) return -1;
 
   const mid = Math.floor((left + right) / 2);
   const midNum = numbers[mid];
@@ -37,5 +37,37 @@ function _recursiveBinarySearch(
   }
 }
 
-const array = [1, 2, 3, 4, 5];
-console.log(recursiveBinarySearch(array, 5));
+function iterativeBinarySearch(numbers: number[], number: number): number {
+  if (numbers.length === 0) return -1;
+
+  let left = 0;
+  let right = numbers.length - 1;
+
+  let mid = getMid();
+  let midNum = numbers[mid];
+
+  let index = -1;
+
+  while (left <= right) {
+    midNum = numbers[mid];
+    if (midNum === number) {
+      index = mid;
+      break;
+    } else if (midNum < number) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+    mid = getMid();
+  }
+
+  return index;
+
+  function getMid() {
+    return Math.floor((left + right) / 2);
+  }
+}
+
+// const array = [1, 2, 3, 4, 5];
+// console.log(recursiveBinarySearch(array, 5));
+// console.log(iterativeBinarySearch(array, 3));
